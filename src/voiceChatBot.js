9ae34bf9d9f8ffdf2b06a65fedb973dfc95db68d
Alex Mercer <john.thor.peterson@gmail.com>
2021-01-15 00:15:17 +0000

    Update voiceChatBot.js

--- src/voiceChatBot.js
+++ src/voiceChatBot.js
@@ -7,7 +7,7 @@
  * 
  * The MIT License (MIT)
  * 
- * Copyright (c) 2020, Matthew Williams
+ * Copyright (c) 2020, Matthew Williams, Alex Mercer
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this
  * software and associated documentation files (the "Software"), to deal in the Software
