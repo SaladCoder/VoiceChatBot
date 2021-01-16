4d3f5aca5021d6ff77587c17eeacaedf8b635270
Alex Mercer <john.thor.peterson@gmail.com>
2021-01-16 00:21:23 +0000

    Update channelCreation.js
    Updated internal notes!

--- src/events/commands/channelCreation.js
+++ src/events/commands/channelCreation.js
@@ -20,32 +20,49 @@ module.exports = {
         const type = args[1].toLowerCase();
         const property = args[2];
 
-        // Check the member type remove
+        // Check we have all 3 args for this command
         if (args.length !== 3) return message.reply(intLang('commands.channelCreation._errors.wrongOption', discord.prefix));
 
+        // Load our Config file for later reading and writing
         const configFile = JSON.parse(fs.readFileSync(`${__dirname}/../../config/config.json`, 'utf8'));
 
         switch(type){
             case 'slots':
+
+                // Convert to a number for comparison
                 const slots = Number(property);
+
+                // If the Number given is the same as the config file, we return making the planet greener
                 if (slots === configFile.channelTemplate.channelSlots) return message.reply(intLang('commands.channelCreation.slots.failed.slotsNotUpdated'));
+
+                // Check if a number was given for arg slots
                 if (typeof slots === 'number') {
+
+                    // Check if the range is not above 99 and not below 1, else return
                     if (!slots > 99 || !slots < 1) configFile.channelTemplate.channelSlots = slots;
                     else return message.reply(intLang('commands.channelCreation.slots._errors.slotsIncorrectValue'));
                 }
                 break;
             case 'lock':
+
+                // If the option [true/false] given is the same as the config file, we return making the planet more greener :D
                 if (property === configFile.channelTemplate.lockStatus) return message.reply(intLang('commands.channelCreation.lock.failed.lockNotUpdated'));
+
+                // Check if we was given a string
                 if (typeof property === 'string') {
+
+                    // Straight forward if statement, if anything other than [true/false] given, return
                     if (property === 'true' || property === 'false') configFile.channelTemplate.lockStatus = property.toLowerCase();
                     else return message.reply(intLang('commands.channelCreation.lock._errors.lockIncorrectType'));
                 }
                 break;
             default:
+
+                // if any of the options given do not equal slots or lock, return here
                 return message.reply(intLang('commands.channelCreation._errors.wrongOption', discord.prefix));
         }
 
-        // Write to file our new Channel ID for the log dumps
+        // Write to file if any changes are accepted
         fs.writeFile(`${__dirname}/../../config/config.json`, JSON.stringify(configFile, null, "\t"), error => {	
             if (error) return logger.error(intLang('commands.channelCreation.failed.unableToWriteFile', error)+ ' [0004]');
 
