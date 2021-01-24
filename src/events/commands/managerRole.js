7dfed191c113ae59c21e16d81c44de042df2c40c
Alex Mercer <john.thor.peterson@gmail.com>
2021-01-24 19:33:13 +0000

    Performance & Code reduction
    Removed 4 unnecessary database searches.
    
    Edited, code reduction for file staffRole and managerRole

--- src/events/commands/managerRole.js
+++ src/events/commands/managerRole.js
@@ -40,20 +40,18 @@ module.exports = {
         switch(option){
             case 'add':
 
-                // Start search for Role or Member
-                dbStaffRoles.find({ staff: manager.id }, (error, isStaff) => {
-                    if (error) return logger.error(intLang('nedb._errors.staffRolesFindIneffective', error)+ ' [0029]'); // Error Handle (╯°□°）╯︵ ┻━┻
-                    messageintLang = (!member) ? 'commands.manager._errors.isStaffRole' : 'commands.manager._errors.isStaffMember';
-                    if (isStaff.length > 0) return message.reply(intLang(messageintLang, discord.prefix, discord.prefix));
-
                     // Check for duplicate entry
-                    dbStaffRoles.findOne({ manager: manager.id }, (error, staffRoles) => {
+                    dbStaffRoles.findOne({ $or: [{ manager: manager.id }, { staff: manager.id }] }, (error, result) => {
                         
                         // Check if a Role or Member is already added
                         if (error) return logger.error(intLang('nedb._errors.staffRolesFindOneIneffective', error)+ ' [0030]'); // Error Handle (╯°□°）╯︵ ┻━┻
-                        messageintLang = (!member) ? 'commands.manager.add.addRoleBotManagerIneffective' : 'commands.manager.add.addBotManagerIneffective';
-                        if (staffRoles !== null) return message.reply(intLang(messageintLang, manager.id, discord.prefix, discord.prefix));
-                        
+
+                        // Check they're not a Staff or Manager member to avoid double entry.
+                        if (result) {
+                            if (result.staff) return message.reply(intLang((!member) ? 'commands.manager._errors.isStaffRole' : 'commands.manager._errors.isStaffMember', discord.prefix, discord.prefix));
+                            if (result.manager) return message.reply(intLang((!member) ? 'commands.manager.add.addRoleBotManagerIneffective' : 'commands.manager.add.addBotManagerIneffective', manager.id, discord.prefix, discord.prefix)); 
+                        }
+
                         // Insert the new Bot manager
                         dbStaffRoles.insert({ manager: manager.id, type: type, roleTitle: title }, error => {
                             if (error) return logger.error(intLang('nedb._errors.staffRolesInsertIneffective', error)+ ' [0031]'); // Error Handle (╯°□°）╯︵ ┻━┻
@@ -62,32 +60,24 @@ module.exports = {
                             message.react('✅')
 
                                 // Assign the right message depending on Role or Member
-                                .then(() => messageintLang = (!member) ? 'commands.manager.add.successRole' : 'commands.manager.add.successMember')
-                                .then(() => message.reply(intLang(messageintLang, manager.id)))
+                                .then(() => message.reply(intLang((!member) ? 'commands.manager.add.successRole' : 'commands.manager.add.successMember', manager.id)))
                                 .then(() => dumpEvent.dumpCommand(client, message, 'black', `${this.name} add`, manager, `${role ? true : false}`))
                                 .catch(() => logger.error(intLang('discord._errors.messageReactIneffective', message.channel.id)+ ' [0032]'))
                         });
                     });
-                });
                 break;
 
             case 'remove':
 
-                // Start search for Role or Member
-                dbStaffRoles.find({ staff: manager.id }, (error, isStaff) => {
-                    if (error) return logger.error(intLang('nedb._errors.staffRolesFindIneffective', error)+ ' [0033]'); // Error Handle (╯°□°）╯︵ ┻━┻
-                    messageintLang = (!member) ? 'commands.manager._errors.isStaffRemoveRole' : 'commands.manager._errors.isStaffRemoveMember';
-                    if (isStaff.length > 0) return message.reply(intLang(messageintLang, discord.prefix, discord.prefix));
-            
                     // Check if entry exists
-                    dbStaffRoles.findOne({ manager: manager.id }, (error, staffRoles) => {
+                    dbStaffRoles.findOne({ $or: [{ manager: manager.id }, { staff: manager.id }] }, (error, result) => {
                         
                         // Check for error and duplicate entry
                         if (error) return logger.error(intLang('nedb._errors.staffRolesFindIneffective', error)+ ' [0034]'); // Error Handle (╯°□°）╯︵ ┻━┻
-
-                        // Assign the right message depending on Role or Member
-                        messageintLang = (!member) ? 'commands.manager.remove.removeBotManagerRoleIneffective' : 'commands.manager.remove.removeBotManagerIneffective';
-                        if (staffRoles === null) return message.reply(intLang(messageintLang, manager.id, discord.prefix, discord.prefix));
+                        
+                        // Check that entry exists and that they're not staff
+                        if (!result) return message.reply(intLang((!member) ? 'commands.manager.remove.removeBotManagerRoleIneffective' : 'commands.manager.remove.removeBotManagerIneffective', manager.id, discord.prefix, discord.prefix));
+                        if (result.staff) return message.reply(intLang((!member) ? 'commands.manager._errors.isStaffRemoveRole' : 'commands.manager._errors.isStaffRemoveMember', discord.prefix, discord.prefix));
                         
                         // Remove a Bot manager
                         dbStaffRoles.remove({ manager: manager.id }, { multi: true }, error => {
@@ -97,15 +87,13 @@ module.exports = {
                             message.react('✅')
 
                                 // Assign the right message depending on Role or Member
-                                .then(() => messageintLang = (!member) ? 'commands.manager.remove.successRole' : 'commands.manager.remove.successMember')
-                                .then(() => message.reply(intLang(messageintLang, manager.id,)))
+                                .then(() => message.reply(intLang((!member) ? 'commands.manager.remove.successRole' : 'commands.manager.remove.successMember', manager.id,)))
                                 .then(() => dumpEvent.dumpCommand(client, message, 'black', `${this.name} remove`, manager, `${role ? true : false}`))
                                 .catch(() => logger.error(intLang('discord._errors.messageReactIneffective', message.channel.id)+ ' [0036]'));
 
 
                         });
                     });
-                });
                 break;
         }
     }
--- src/events/commands/staffRole.js
+++ src/events/commands/staffRole.js
@@ -19,6 +19,7 @@ module.exports = {
         // Member and Role Mention Verification
         const member = message.mentions.members.first() || message.guild.members.cache.get(arg[1]);
         const role = message.mentions.roles.first() || message.guild.roles.cache.get(arg[1]);
+
         if (typeof member !== 'undefined' && member.id === message.author.id) return message.reply(intLang('commands.staff._errors.selfMember'));
         if (!member && !role) return message.reply(intLang('commands.staff._errors.invalidMemberOrRole'));
 
@@ -40,55 +41,45 @@ module.exports = {
         switch(option){
             case 'add':
 
-                // Start search for Role or Member
-                dbStaffRoles.find({ manager: staff.id }, (error, isManager) => {
-                    if (error) return logger.error(intLang('nedb._errors.staffRolesFindIneffective', error)+ ' [0079]'); // Error Handle (╯°□°）╯︵ ┻━┻
-                    messageintLang = (!member) ? 'commands.staff._errors.isManagerRole' : 'commands.staff._errors.isManagerMember';
-                    if (isManager.length > 0) return message.reply(intLang(messageintLang, discord.prefix, discord.prefix));
+                // Check if entry exists
+                dbStaffRoles.findOne({ $or: [{ manager: staff.id }, { staff: staff.id }] }, (error, result) => {
+                    
+                    // Check if a Role or Member is already added
+                    if (error) return logger.error(intLang('nedb._errors.staffRolesFindOneIneffective', error)+ ' [0080]'); // Error Handle (╯°□°）╯︵ ┻━┻
 
-                    // Check for duplicate entry
-                    dbStaffRoles.findOne({ staff: staff.id }, (error, staffRoles) => {
-                        
-                        // Check if a Role or Member is already added
-                        if (error) return logger.error(intLang('nedb._errors.staffRolesFindOneIneffective', error)+ ' [0080]'); // Error Handle (╯°□°）╯︵ ┻━┻
-                        messageintLang = (!member) ? 'commands.staff.add.addRoleStaffIneffective' : 'commands.staff.add.addStaffIneffective';
-                        if (staffRoles !== null) return message.reply(intLang(messageintLang, staff.id, discord.prefix, discord.prefix));
-                        
-                        // Insert the new Staff member
-                        dbStaffRoles.insert({ staff: staff.id, type: type, roleTitle: title }, error => {
-                            if (error) return logger.error(intLang('nedb._errors.staffRolesInsertIneffective', error)+ ' [0081]'); // Error Handle (╯°□°）╯︵ ┻━┻
+                    // Check they're not a Manager or Staff member to avoid double entry.
+                    if (result) {
+                        if (result.manager) return message.reply(intLang((!member) ? 'commands.staff._errors.isManagerRole' : 'commands.staff._errors.isManagerMember', discord.prefix, discord.prefix));
+                        if (result.staff) return message.reply(intLang((!member) ? 'commands.staff.add.addRoleStaffIneffective' : 'commands.staff.add.addStaffIneffective', staff.id, discord.prefix, discord.prefix));
+                    }
+                    
+                    // Insert the new Staff member
+                    dbStaffRoles.insert({ staff: staff.id, type: type, roleTitle: title }, error => {
+                        if (error) return logger.error(intLang('nedb._errors.staffRolesInsertIneffective', error)+ ' [0081]'); // Error Handle (╯°□°）╯︵ ┻━┻
 
-                            // Success Response ┬─┬ ノ( ゜-゜ノ)
-                            message.react('✅')
+                        // Success Response ┬─┬ ノ( ゜-゜ノ)
+                        message.react('✅')
 
-                                // Assign the right message depending on Role or Member
-                                .then(() => messageintLang = (!member) ? 'commands.staff.add.successRole' : 'commands.staff.add.successMember')
-                                .then(() => message.reply(intLang(messageintLang, staff.id)))
-                                .then(() => dumpEvent.dumpCommand(client, message, 'black', `${this.name} add`, staff, `${role ? true : false}`))
-                                .catch(() => logger.error(intLang('discord._errors.messageReactIneffective', message.channel.id)+ ' [0082]'));
-                        });
+                            // Assign the right message depending on Role or Member
+                            .then(() => message.reply(intLang((!member) ? 'commands.staff.add.successRole' : 'commands.staff.add.successMember', staff.id)))
+                            .then(() => dumpEvent.dumpCommand(client, message, 'black', `${this.name} add`, staff, `${role ? true : false}`))
+                            .catch(() => logger.error(intLang('discord._errors.messageReactIneffective', message.channel.id)+ ' [0082]'));
                     });
                 });
                 break;
 
             case 'remove':
 
-                // Start search for Role or Member
-                dbStaffRoles.find({ manager: staff.id }, (error, isManager) => {
-                    if (error) return logger.error(intLang('nedb._errors.staffRolesFindIneffective', error)+ ' [0083]'); // Error Handle (╯°□°）╯︵ ┻━┻
-                    messageintLang = (!member) ? 'commands.staff._errors.isManagerRemoveRole' : 'commands.staff._errors.isManagerRemoveMember';
-                    if (isManager.length > 0) return message.reply(intLang(messageintLang, discord.prefix, discord.prefix));
-
                     // Check if entry exists
-                    dbStaffRoles.findOne({ staff: staff.id }, (error, staffRoles) => {
+                    dbStaffRoles.findOne({ $or: [{ manager: staff.id }, { staff: staff.id }] }, (error, result) => {
                         
                         // Check for error and duplicate entry
                         if (error) return logger.error(intLang('nedb._errors.staffRolesFindIneffective', error)+ ' [0084]'); // Error Handle (╯°□°）╯︵ ┻━┻
 
-                        // Assign the right message depending on Role or Member
-                        messageintLang = (!member) ? 'commands.staff.remove.removeStaffRoleIneffective' : 'commands.staff.remove.removeStaffIneffective';
-                        if (staffRoles === null) return message.reply(intLang(messageintLang, staff.id, discord.prefix, discord.prefix));
-                        
+                        // Check that entry exists and that they're not manager
+                        if (!result) return message.reply(intLang((!member) ? 'commands.staff.remove.removeStaffRoleIneffective' : 'commands.staff.remove.removeStaffIneffective', staff.id, discord.prefix, discord.prefix));
+                        if (result.manager) return message.reply(intLang((!member) ? 'commands.staff._errors.isManagerRemoveRole' : 'commands.staff._errors.isManagerRemoveMember', discord.prefix, discord.prefix));
+
                         // Remove a Staff member
                         dbStaffRoles.remove({ staff: staff.id }, { multi: true }, error => {
                             if (error) return logger.error(intLang('nedb._errors.staffRolesRemoveIneffective', error)+ ' [0085]'); // Error Handle (╯°□°）╯︵ ┻━┻
@@ -97,13 +88,11 @@ module.exports = {
                             message.react('✅')
 
                                 // Assign the right message depending on Role or Member
-                                .then(() => messageintLang = (!member) ? 'commands.staff.remove.successRole' : 'commands.staff.remove.successMember', )
-                                .then(() => message.reply(intLang(messageintLang, staff.id,)))
+                                .then(() => message.reply(intLang((!member) ? 'commands.staff.remove.successRole' : 'commands.staff.remove.successMember', staff.id)))
                                 .then(() => dumpEvent.dumpCommand(client, message, 'black', `${this.name} remove`, staff, `${role ? true : false}`))
                                 .catch(() => logger.error(intLang('discord._errors.messageReactIneffective', message.channel.id)+ ' [0086]'));
                         });
                     });
-                });
                 break;
         }
     }
